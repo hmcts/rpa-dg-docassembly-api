361a18a7ea19a2cdd447b2c56523a595ed28a201
Linus Norton <linusnorton@gmail.com>
2019-03-18 19:03:14 +0000

    debug

--- src/main/java/uk/gov/hmcts/reform/dg/docassembly/service/DmStoreUploader.java
+++ src/main/java/uk/gov/hmcts/reform/dg/docassembly/service/DmStoreUploader.java
@@ -36,6 +36,7 @@ public class DmStoreUploader {
     }
 
     public CreateTemplateRenditionDto uploadFile(File file, CreateTemplateRenditionDto createTemplateRenditionDto) {
+        System.out.println("Uploading to DM store: " + dmStoreAppBaseUrl);
         if (createTemplateRenditionDto.getRenditionOutputLocation() != null) {
             uploadNewDocumentVersion(file, createTemplateRenditionDto);
         } else {
