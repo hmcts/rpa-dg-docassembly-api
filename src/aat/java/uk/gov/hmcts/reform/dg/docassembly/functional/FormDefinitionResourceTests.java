01faba6c3abe9ee7d542a65ff4e867a1707ddf3a
Linus Norton <linusnorton@gmail.com>
2019-03-20 09:28:48 +0000

    Remove idam client

--- src/aat/java/uk/gov/hmcts/reform/dg/docassembly/functional/FormDefinitionResourceTests.java
+++ src/aat/java/uk/gov/hmcts/reform/dg/docassembly/functional/FormDefinitionResourceTests.java
@@ -1,52 +1,21 @@
 package uk.gov.hmcts.reform.dg.docassembly.functional;
 
-import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit4.SpringRunner;
-import uk.gov.hmcts.reform.dg.docassembly.Application;
 import uk.gov.hmcts.reform.dg.docassembly.testutil.Env;
-import uk.gov.hmcts.reform.dg.docassembly.testutil.IdamHelper;
-import uk.gov.hmcts.reform.dg.docassembly.testutil.S2sHelper;
+import uk.gov.hmcts.reform.dg.docassembly.testutil.TestUtil;
 
 import static uk.gov.hmcts.reform.dg.docassembly.testutil.Base64.base64;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest(classes = Application.class, properties = "SpringBootTest")
-@TestPropertySource(locations = "classpath:application-aat.yaml")
-@ActiveProfiles("aat")
 public class FormDefinitionResourceTests {
 
-    @Autowired
-    private IdamHelper idamHelper;
-
-    @Autowired
-    private S2sHelper s2sHelper;
-
-    private String idamAuth;
-    private String s2sAuth;
-
-    @Before
-    public void setup() {
-        idamAuth = idamHelper.getIdamToken();
-        s2sAuth = s2sHelper.getS2sToken();
-
-        RestAssured.useRelaxedHTTPSValidation();
-    }
+    TestUtil testUtil = new TestUtil();
 
     @Test
     public void testFormDefinitionGetTemplateWithUIDefinition() {
-
-        Response response = RestAssured.given()
-            .header("Authorization", idamAuth)
-            .header("ServiceAuthorization", s2sAuth)
+        Response response = testUtil
+            .authRequest()
             .request("GET",
                 Env.getTestUrl()
                 + "/api/form-definitions/"
@@ -60,10 +29,8 @@ public class FormDefinitionResourceTests {
 
     @Test
     public void testFormDefinitionGetNotExistingTemplate() {
-
-        Response response = RestAssured.given()
-            .header("Authorization", idamAuth)
-            .header("ServiceAuthorization", s2sAuth)
+        Response response = testUtil
+            .authRequest()
             .request("GET",
                 Env.getTestUrl()
                 + "/api/form-definitions/"
@@ -77,10 +44,8 @@ public class FormDefinitionResourceTests {
 
     @Test
     public void testFormDefinitionGetTemplateWithoutUIDefinition() {
-
-        Response response = RestAssured.given()
-            .header("Authorization", idamAuth)
-            .header("ServiceAuthorization", s2sAuth)
+        Response response = testUtil
+            .authRequest()
             .request("GET",
                 Env.getTestUrl()
                 + "/api/form-definitions/"
