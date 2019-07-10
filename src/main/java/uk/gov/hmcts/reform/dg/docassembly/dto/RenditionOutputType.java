ddd533b0d06b2a706459bba0e6a47f0d0807f8b4
benouaer <tech@benouaer.com>
2019-07-10 12:52:22 +0100

    enable support for html and text as output formats

--- src/main/java/uk/gov/hmcts/reform/dg/docassembly/dto/RenditionOutputType.java
+++ src/main/java/uk/gov/hmcts/reform/dg/docassembly/dto/RenditionOutputType.java
@@ -3,6 +3,8 @@ package uk.gov.hmcts.reform.dg.docassembly.dto;
 public enum RenditionOutputType {
 
     PDF(".pdf", "application/pdf"),
+    HTML(".html", "text/html"),
+    TEXT(".txt", "text/plain"),
     DOC(".doc", "application/msword"),
     DOCX(".docx", "application/vnd.openxmlformats-officedocument.wordprocessingml.document");
 
