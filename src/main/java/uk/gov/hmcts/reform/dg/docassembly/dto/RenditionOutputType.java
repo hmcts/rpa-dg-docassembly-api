13e842df09dab5101a2b9f738e33e4f02ee6a94b
benouaer <tech@benouaer.com>
2019-07-10 12:58:03 +0100

    add support for rtf output format

--- src/main/java/uk/gov/hmcts/reform/dg/docassembly/dto/RenditionOutputType.java
+++ src/main/java/uk/gov/hmcts/reform/dg/docassembly/dto/RenditionOutputType.java
@@ -5,6 +5,7 @@ public enum RenditionOutputType {
     PDF(".pdf", "application/pdf"),
     HTML(".html", "text/html"),
     TEXT(".txt", "text/plain"),
+    RTF(".rtf", "application/rtf"),
     DOC(".doc", "application/msword"),
     DOCX(".docx", "application/vnd.openxmlformats-officedocument.wordprocessingml.document");
 
