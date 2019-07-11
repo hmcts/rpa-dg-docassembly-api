86d728b641864941b927beb43f47450fc94b9034
benouaer <tech@benouaer.com>
2019-07-11 10:54:14 +0100

    revert output format additions as not compatible with docstore

--- src/main/java/uk/gov/hmcts/reform/dg/docassembly/dto/RenditionOutputType.java
+++ src/main/java/uk/gov/hmcts/reform/dg/docassembly/dto/RenditionOutputType.java
@@ -3,9 +3,6 @@ package uk.gov.hmcts.reform.dg.docassembly.dto;
 public enum RenditionOutputType {
 
     PDF(".pdf", "application/pdf"),
-    HTML(".html", "text/html"),
-    TEXT(".txt", "text/plain"),
-    RTF(".rtf", "application/rtf"),
     DOC(".doc", "application/msword"),
     DOCX(".docx", "application/vnd.openxmlformats-officedocument.wordprocessingml.document");
 
