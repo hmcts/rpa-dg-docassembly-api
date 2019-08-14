e3681f7b3b1dc8f5465d6168dc102e39dc4a6b9a
Russell Ward <russell.ward@inkltd.co.uk>
2019-08-14 16:31:01 +0100

    DEBUG commit, reverted JWTDto to see if AAT works

--- src/main/java/uk/gov/hmcts/reform/dg/docassembly/dto/JwtDto.java
+++ src/main/java/uk/gov/hmcts/reform/dg/docassembly/dto/JwtDto.java
@@ -10,10 +10,12 @@ public class JwtDto {
 
     public void setJwt(String jwt) {
         // Avoid non JWT Tokens being set from the header (Fix for SonarCloud https://sonarcloud.io/organizations/hmcts/rules?open=javasecurity%3AS2083&rule_key=javasecurity%3AS2083)
-        if (jwt.matches("^[A-Za-z0-9-_=]+\\.[A-Za-z0-9-_=]+\\.?[A-Za-z0-9-_.+/=]*$")) {
-            this.jwt = jwt;
-        } else {
-            this.jwt = "";
-        }
+//        if (jwt.matches("^[A-Za-z0-9-_=]+\\.[A-Za-z0-9-_=]+\\.?[A-Za-z0-9-_.+/=]*$")) {
+//            this.jwt = jwt;
+//        } else {
+//            this.jwt = "";
+//        }
+        // TEMPORARY FOR DEBUG!
+        this.jwt = jwt;
     }
 }
