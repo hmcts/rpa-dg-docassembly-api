d7c379e38e5f94db2a93920be468f741c3656a34
Russell Ward <russell.ward@inkltd.co.uk>
2019-08-14 16:46:21 +0100

    Basically reverting JWtDTo to mirror master . . . just to see if it builds

--- src/main/java/uk/gov/hmcts/reform/dg/docassembly/dto/JwtDto.java
+++ src/main/java/uk/gov/hmcts/reform/dg/docassembly/dto/JwtDto.java
@@ -9,13 +9,6 @@ public class JwtDto {
     }
 
     public void setJwt(String jwt) {
-        // Avoid non JWT Tokens being set from the header (Fix for SonarCloud https://sonarcloud.io/organizations/hmcts/rules?open=javasecurity%3AS2083&rule_key=javasecurity%3AS2083)
-//        if (jwt.matches("^[A-Za-z0-9-_=]+\\.[A-Za-z0-9-_=]+\\.?[A-Za-z0-9-_.+/=]*$")) {
-//            this.jwt = jwt;
-//        } else {
-//            this.jwt = "";
-//        }
-        // TEMPORARY FOR DEBUG!
         this.jwt = jwt;
     }
 }
