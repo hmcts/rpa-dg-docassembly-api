ac71882e740044d4d9b113439338df7986644c04
Russell Ward <russell.ward@inkltd.co.uk>
2019-08-19 11:35:17 +0100

    On investigation noted that it is not a true JWT Token, and have therefore opted to input validate that it is at least a Base64 Encoded String

--- src/main/java/uk/gov/hmcts/reform/dg/docassembly/dto/JwtDto.java
+++ src/main/java/uk/gov/hmcts/reform/dg/docassembly/dto/JwtDto.java
@@ -9,6 +9,12 @@ public class JwtDto {
     }
 
     public void setJwt(String jwt) {
-        this.jwt = jwt;
+        // Avoid Tokens being set from the header without validation (Fix for SonarCloud https://sonarcloud.io/organizations/hmcts/rules?open=javasecurity%3AS2083&rule_key=javasecurity%3AS2083)
+        // This object does not hold a true JWT Token, but it does hold a Base64 encoded string, thus, if it is Base64 encoded, we allow it through.
+        if(jwt.matches("^[A-Za-z0-9-_.+/=]+$")){
+            this.jwt = jwt;
+        } else {
+            this.jwt = "";
+        }
     }
 }
