35e961fee33a4092aee03c9d20aaa6f53ecdf638
Pawel Kaczmarek <pawel.kaczmarek@gmail.com>
2019-11-28 17:38:38 +0100

    em test helper (#87)
    * em test helper
    
    * fixes
    
    * fixes
    
    * fixes
    
    * fix

--- src/aat/java/uk/gov/hmcts/reform/dg/docassembly/functional/BaseTest.java
+++ src/aat/java/uk/gov/hmcts/reform/dg/docassembly/functional/BaseTest.java
@@ -1,34 +1,25 @@
-package uk.gov.hmcts.reform.dg.docassembly.smoke;
+package uk.gov.hmcts.reform.dg.docassembly.functional;
 
-import io.restassured.RestAssured;
-import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import uk.gov.hmcts.reform.dg.docassembly.testutil.TestUtil;
+import uk.gov.hmcts.reform.dg.docassembly.testutil.ToggleProperties;
 import uk.gov.hmcts.reform.em.EmTestConfig;
-
 
 @SpringBootTest(classes = {TestUtil.class, EmTestConfig.class})
 @PropertySource(value = "classpath:application.yml")
 @RunWith(SpringRunner.class)
-public class SmokeTest {
+@EnableConfigurationProperties(ToggleProperties.class)
+public abstract class BaseTest {
 
     @Autowired
-    private TestUtil testUtil;
+    TestUtil testUtil;
 
-    @Test
-    public void testHealthEndpoint() {
+    @Autowired
+    ToggleProperties toggleProperties;
 
-        RestAssured.useRelaxedHTTPSValidation();
-
-        RestAssured.given()
-            .request("GET", testUtil.getTestUrl() + "/health")
-            .then()
-            .statusCode(200);
-
-
-    }
 }
