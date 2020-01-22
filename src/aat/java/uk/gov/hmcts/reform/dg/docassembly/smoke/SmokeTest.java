1d1945e52fef0161066249d392307a121991a6a1
yogesh-hullatti <yogesh.hullatti@hmcts.net>
2020-01-22 20:57:37 +0000

    Removed unused import

--- src/aat/java/uk/gov/hmcts/reform/dg/docassembly/smoke/SmokeTest.java
+++ src/aat/java/uk/gov/hmcts/reform/dg/docassembly/smoke/SmokeTest.java
@@ -1,7 +1,6 @@
 package uk.gov.hmcts.reform.dg.docassembly.smoke;
 
 import io.restassured.RestAssured;
-import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import uk.gov.hmcts.reform.dg.docassembly.testutil.TestUtil;
 
