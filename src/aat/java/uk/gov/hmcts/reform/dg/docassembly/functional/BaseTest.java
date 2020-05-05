0049e77ae4fad8561c341a677993d7b1b33843f1
Kasi Subramaniam <Kasi.Subramaniam@HMCTS.NET>
2020-05-05 18:23:06 +0100

    EM-2723 Serenity Reporting

--- src/aat/java/uk/gov/hmcts/reform/dg/docassembly/functional/BaseTest.java
+++ src/aat/java/uk/gov/hmcts/reform/dg/docassembly/functional/BaseTest.java
@@ -1,18 +1,18 @@
 package uk.gov.hmcts.reform.dg.docassembly.functional;
 
+import net.serenitybdd.junit.spring.integration.SpringIntegrationSerenityRunner;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.test.context.junit4.SpringRunner;
 import uk.gov.hmcts.reform.dg.docassembly.testutil.TestUtil;
 import uk.gov.hmcts.reform.dg.docassembly.testutil.ToggleProperties;
 import uk.gov.hmcts.reform.em.EmTestConfig;
 
 @SpringBootTest(classes = {TestUtil.class, EmTestConfig.class})
 @PropertySource(value = "classpath:application.yml")
-@RunWith(SpringRunner.class)
+@RunWith(SpringIntegrationSerenityRunner.class)
 @EnableConfigurationProperties(ToggleProperties.class)
 public abstract class BaseTest {
 
