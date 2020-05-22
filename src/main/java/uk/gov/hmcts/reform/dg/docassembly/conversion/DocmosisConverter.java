3d242f49cf11a74335ecf44e22d663d9a6ebdc7a
isha2040 <58481974+isha2040@users.noreply.github.com>
2020-05-22 18:56:58 +0100

    EM-2954 Changes for Converting documents using Docmosis         Introduced some correction in current configuration

---
+++ src/main/java/uk/gov/hmcts/reform/dg/docassembly/conversion/DocmosisConverter.java
@@ -0,0 +1,89 @@
+package uk.gov.hmcts.reform.dg.docassembly.conversion;
+
+import okhttp3.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
+@Service
+public class DocmosisConverter {
+    private final Logger log = LoggerFactory.getLogger(DocmosisConverter.class);
+    private static final String PDF_CONTENT_TYPE = "application/pdf";
+    private final String docmosisAccessKey;
+    private final String docmosisConvertEndpoint;
+    private final OkHttpClient httpClient;
+
+    public DocmosisConverter(@Value("${docmosis.accessKey}") String docmosisAccessKey,
+                             @Value("${docmosis.convert.endpoint}") String docmosisConvertEndpoint,
+                             OkHttpClient httpClient) {
+        this.docmosisAccessKey = docmosisAccessKey;
+        this.docmosisConvertEndpoint = docmosisConvertEndpoint;
+        this.httpClient = httpClient;
+    }
+
+    /**
+     * Converting file to pdf.
+     *
+     * @param file File to be converted into PDF
+     * @return the converted file
+     */
+    private File convert(File file) throws IOException {
+        final Request request = this.createRequest(file);
+        final Response response = httpClient.newCall(request).execute();
+
+        if (!response.isSuccessful()) {
+            throw new IOException(String.format("Docmosis error (%s) converting: %s", response.code(), file.getName()));
+        }
+        return createConvertedFile(response);
+    }
+
+    /**
+     * Calls the Docmosis api to convert the document.
+     *
+     * @param file File to be converted into PDF     *
+     * @return Request Object
+     *
+     */
+    private Request createRequest(final File file) {
+        final String originalFileName = file.getName();
+        final String convertedFileName = originalFileName + ".pdf";
+
+        MultipartBody requestBody = new MultipartBody
+            .Builder()
+            .setType(MultipartBody.FORM)
+            .addFormDataPart("accessKey", docmosisAccessKey)
+            .addFormDataPart("outputName", convertedFileName)
+            .addFormDataPart("file", originalFileName, RequestBody.create(MediaType.get(PDF_CONTENT_TYPE), file))
+            .build();
+
+        return new Request.Builder()
+            .header("Accept", PDF_CONTENT_TYPE)
+            .url(docmosisConvertEndpoint)
+            .method("POST", requestBody)
+            .build();
+    }
+
+    /**
+     * Calls the Docmosis api to convert the document.
+     * @return Request converted file
+     *
+     */
+    private File createConvertedFile(Response response) throws IOException {
+        final File convertedFile = File.createTempFile("converted-document", ".pdf");
+
+        Files.write(convertedFile.toPath(), response.body().bytes());
+
+        return convertedFile;
+    }
+
+
+    public File convertFileToPDF(File originalFile) throws IOException {
+
+        return convert(originalFile);
+    }
+}
