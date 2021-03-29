b3213eb80633fd636678690b5bcae9170e48ae9c
rajkatla-hmcts <72351979+rajkatla-hmcts@users.noreply.github.com>
2021-03-29 11:16:53 +0100

    EM-3638 - EM-dg-docassembly-api: Add functional tests for TemplateRenditionResource (#307)
    Co-authored-by: yogesh-hullatti <53007108+yogesh-hullatti@users.noreply.github.com>

--- src/aat/java/uk/gov/hmcts/reform/dg/docassembly/functional/TemplateRenditionResourceTests.java
+++ src/aat/java/uk/gov/hmcts/reform/dg/docassembly/functional/TemplateRenditionResourceTests.java
@@ -1,11 +1,11 @@
 package uk.gov.hmcts.reform.dg.docassembly.functional;
 
-import io.restassured.response.Response;
-import org.junit.Assert;
-import org.junit.Assume;
-import org.junit.Rule;
-import org.junit.Test;
+import io.restassured.specification.RequestSpecification;
+import org.junit.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import uk.gov.hmcts.reform.dg.docassembly.dto.CreateTemplateRenditionDto;
+import uk.gov.hmcts.reform.dg.docassembly.testutil.TestUtil;
 import uk.gov.hmcts.reform.document.domain.Document;
 import uk.gov.hmcts.reform.em.test.retry.RetryRule;
 
@@ -14,25 +14,46 @@ import static uk.gov.hmcts.reform.dg.docassembly.testutil.Base64.base64;
 
 public class TemplateRenditionResourceTests extends BaseTest {
 
+    @Autowired
+    private TestUtil testUtil;
+
+    @Value("${test.url}")
+    private String testUrl;
+
     @Rule
     public RetryRule retryRule = new RetryRule(3);
+
+    private RequestSpecification request;
+    private RequestSpecification unAuthenticatedRequest;
+
+    @Before
+    public void setupRequestSpecification() {
+        request = testUtil
+                .authRequest()
+                .baseUri(testUrl)
+                .contentType(APPLICATION_JSON_VALUE);
+
+        unAuthenticatedRequest = testUtil
+                .unAuthenticatedRequest()
+                .baseUri(testUrl)
+                .contentType(APPLICATION_JSON_VALUE);
+    }
 
     @Test
     public void testTemplateRendition() {
         // If the Endpoint Toggles are enabled, continue, if not skip and ignore
         Assume.assumeTrue(toggleProperties.isEnableTemplateRenditionEndpoint());
 
-        Response response =
-                testUtil
-                        .authRequest()
-                        .baseUri(testUtil.getTestUrl())
-                        .contentType(APPLICATION_JSON_VALUE)
-                        .body("{\"formPayload\":{\"a\":1}, \"templateId\":\""
-                                + base64("FL-FRM-APP-ENG-00002.docx")
-                                + "\"}")
-                        .post("/api/template-renditions");
-
-        Assert.assertEquals(200, response.getStatusCode());
+        request
+                .body("{\"formPayload\":{\"a\":1}, \"templateId\":\""
+                        + base64("FL-FRM-APP-ENG-00002.docx")
+                        + "\"}")
+                .post("/api/template-renditions")
+                .then()
+                .assertThat()
+                .statusCode(200)
+                .log()
+                .all();
     }
 
     @Test
@@ -40,17 +61,15 @@ public class TemplateRenditionResourceTests extends BaseTest {
         // If the Endpoint Toggles are enabled, continue, if not skip and ignore
         Assume.assumeTrue(toggleProperties.isEnableTemplateRenditionEndpoint());
 
-        Response response =
-                testUtil
-                        .authRequest()
-                        .baseUri(testUtil.getTestUrl())
-                        .contentType(APPLICATION_JSON_VALUE)
-                        .body("{\"formPayload\":{\"a\":1}, \"outputType\":\"DOC\", \"templateId\":\""
-                                + base64("FL-FRM-APP-ENG-00002.docx")
-                                + "\"}")
-                        .post("/api/template-renditions");
-
-        Assert.assertEquals(200, response.getStatusCode());
+        request
+                .body("{\"formPayload\":{\"a\":1}, \"outputType\":\"DOC\", \"templateId\":\""
+                        + base64("FL-FRM-APP-ENG-00002.docx")
+                        + "\"}")
+                .post("/api/template-renditions").then()
+                .assertThat()
+                .statusCode(200)
+                .log()
+                .all();
     }
 
     @Test
@@ -58,17 +77,15 @@ public class TemplateRenditionResourceTests extends BaseTest {
         // If the Endpoint Toggles are enabled, continue, if not skip and ignore
         Assume.assumeTrue(toggleProperties.isEnableTemplateRenditionEndpoint());
 
-        Response response =
-                testUtil
-                        .authRequest()
-                        .baseUri(testUtil.getTestUrl())
-                        .contentType(APPLICATION_JSON_VALUE)
-                        .body("{\"formPayload\":{\"a\":1}, \"outputType\":\"DOCX\", \"templateId\":\""
-                                + base64("FL-FRM-APP-ENG-00002.docx")
-                                + "\"}")
-                        .post("/api/template-renditions");
-
-        Assert.assertEquals(200, response.getStatusCode());
+        request
+                .body("{\"formPayload\":{\"a\":1}, \"outputType\":\"DOCX\", \"templateId\":\""
+                        + base64("FL-FRM-APP-ENG-00002.docx")
+                        + "\"}")
+                .post("/api/template-renditions").then()
+                .assertThat()
+                .statusCode(200)
+                .log()
+                .all();
     }
 
     @Test
@@ -77,10 +94,7 @@ public class TemplateRenditionResourceTests extends BaseTest {
         Assume.assumeTrue(toggleProperties.isEnableTemplateRenditionEndpoint());
 
         CreateTemplateRenditionDto response =
-                testUtil
-                        .authRequest()
-                        .baseUri(testUtil.getTestUrl())
-                        .contentType(APPLICATION_JSON_VALUE)
+                request
                         .body("{\"formPayload\":{\"a\":1}, \"outputType\":\"DOCX\", \"outputFilename\":\"test-output-name\", \"templateId\":\""
                                 + base64("FL-FRM-APP-ENG-00002.docx")
                                 + "\"}")
@@ -95,5 +109,52 @@ public class TemplateRenditionResourceTests extends BaseTest {
         Document doc = testUtil.getDocumentMetadata(dmStoreHref.substring(dmStoreHref.lastIndexOf("/") + 1));
 
         Assert.assertEquals("test-output-name.docx", doc.originalDocumentName);
+    }
+
+    @Test
+    public void shouldReturn500WhenMandatoryFormPayloadIsMissing() {
+        // If the Endpoint Toggles are enabled, continue, if not skip and ignore
+        Assume.assumeTrue(toggleProperties.isEnableTemplateRenditionEndpoint());
+
+        request
+                .body("{\"templateId\":\"" + base64("FL-FRM-APP-ENG-00002.docx") + "\"}")
+                .post("/api/template-renditions")
+                .then()
+                .assertThat()
+                .statusCode(500)//FIXME should be bad request
+                .log()
+                .all();
+    }
+
+    @Test
+    public void shouldReturn500WhenMandatoryTemplateIdIsMissing() {
+        // If the Endpoint Toggles are enabled, continue, if not skip and ignore
+        Assume.assumeTrue(toggleProperties.isEnableTemplateRenditionEndpoint());
+
+        request
+                .body("{\"formPayload\":{\"a\":1}}")
+                .post("/api/template-renditions")
+                .then()
+                .assertThat()
+                .statusCode(500)//FIXME should be bad request
+                .log()
+                .all();
+    }
+
+    @Test
+    public void shouldReturn401WhenUnAthenticateUserPostRequest() {
+        // If the Endpoint Toggles are enabled, continue, if not skip and ignore
+        Assume.assumeTrue(toggleProperties.isEnableTemplateRenditionEndpoint());
+
+        unAuthenticatedRequest
+                .body("{\"formPayload\":{\"a\":1}, \"templateId\":\""
+                        + base64("FL-FRM-APP-ENG-00002.docx")
+                        + "\"}")
+                .post("/api/template-renditions")
+                .then()
+                .assertThat()
+                .statusCode(401)
+                .log()
+                .all();
     }
 }
