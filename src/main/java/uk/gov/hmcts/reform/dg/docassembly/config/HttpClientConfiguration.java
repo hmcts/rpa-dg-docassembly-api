c820abbd12263343ef006de3264cf409e284e078
yogesh-hullatti <yogesh.hullatti@hmcts.net>
2021-04-30 16:40:29 +0100

    Added in logic to increase Timeout

--- src/main/java/uk/gov/hmcts/reform/dg/docassembly/config/HttpClientConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/dg/docassembly/config/HttpClientConfiguration.java
@@ -1,15 +1,25 @@
 package uk.gov.hmcts.reform.dg.docassembly.config;
 
 import okhttp3.OkHttpClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.TimeUnit;
 
 @Configuration
 public class HttpClientConfiguration {
 
+    @Value("${httpclient.call-timeout}")
+    private int timeout;
+
     @Bean
     public OkHttpClient okHttpClient() {
-        return new OkHttpClient();
+        return new OkHttpClient.Builder()
+            .connectTimeout(timeout, TimeUnit.MILLISECONDS)
+            .readTimeout(timeout, TimeUnit.MILLISECONDS)
+            .writeTimeout(timeout, TimeUnit.MILLISECONDS)
+            .build();
     }
 
 }
